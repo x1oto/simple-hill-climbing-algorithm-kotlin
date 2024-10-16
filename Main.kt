bb3089be1e56188241a4db9b00b7d14b04a4ae5b
Nick <89790661+x1oto@users.noreply.github.com>
2024-10-16 21:42:34 +0300

    Create Main.kt

---
+++ Main.kt
@@ -0,0 +1,5 @@
+fun main(args: Array<String>) {
+    // Create an instance of the SimpleHillClimbingAlgorithm class.
+    // This algorithm attempts to evolve a string toward the target string "FIND ME".
+    val simpleHillClimbingAlgorithm = SimpleHillClimbingAlgorithm().run("FIND ME")
+}
